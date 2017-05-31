389a3fecf7ac16965166947172fccea7defa289f
bad4iz <bad4iz@gmail.com>
2017-05-31 22:32:23 +0400

    note editor

--- src/component/smart-notes/noteEditor/NoteEditor.js
+++ src/component/smart-notes/noteEditor/NoteEditor.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {CardTitle, CardActions, Button, Card, CardText, Textfield} from 'react-mdl';
 
 class NoteEditor extends Component {
-    state = {  }
+    state = {}
     render() {
         return (
-            <div>
-                NOTE EDITOR
-            </div>
+            <Card
+                shadow={0}
+                style={{
+                width: '520px',
+                margin: '10px auto'
+            }}>
+                <CardTitle expand>Добавте заметку</CardTitle>
+                <CardText>
+                    <Textfield
+                        onChange={() => {}}
+                        label="Text lines..."
+                        rows={3}
+                        style={{
+                        width: '200px'
+                    }}/>
+                </CardText>
+                <CardActions border>
+                    <Button colored>добавить</Button>
+                </CardActions>
+            </Card>
         );
     }
 }
