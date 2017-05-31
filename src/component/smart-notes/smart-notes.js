4d51d3fafbafeb0e507fe672116a3c692d39c80e
bad4iz <bad4iz@gmail.com>
2017-05-31 22:12:28 +0400

    sceleton notes

--- src/component/smart-notes/smart-notes.js
+++ src/component/smart-notes/smart-notes.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {CardTitle, CardActions, Button, Card, CardText } from 'react-mdl';
+
+import NoteEdinor from './noteEditor/NoteEditor';
+import NotesGrid from './notesGrid/NotesGrid';
 
 class SmartNotes extends Component {
     static path = '/smart-notes';
@@ -7,15 +9,8 @@ class SmartNotes extends Component {
     render() {
         return (
             <div>
-                <Card shadow={0} style={{ width: '320px', height: '320px', margin: '10px auto' }}>
-                    <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
-                    <CardText>
-                        Далеко-далеко за словесными горами тексты, маленький, собрал предложения, деревни рекламных ты, инициал живет свой пояс.
-                    </CardText>
-                    <CardActions border>
-                        <Button colored>View Updates</Button>
-                    </CardActions>
-                </Card>
+               <NoteEdinor/>
+               <NotesGrid/>
             </div>
         );
     }
