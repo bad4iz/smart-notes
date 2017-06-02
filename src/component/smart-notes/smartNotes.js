fe634883de4e48e6c37f93ec31c0db03dc03e79e
Евгений Плотников <bad4iz@gmail.com>
2017-06-02 16:48:28 +0400

    switch

---
+++ src/component/smart-notes/smartNotes.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import NoteEdinor from './noteEditor/NoteEditor';
+import NotesGrid from './notesGrid/NotesGrid';
+
+class SmartNotes extends Component {
+    static path = '/smart-notes';
+    state = {
+        notes: []
+    };
+
+    handleNoteAdd(newNote) {
+        const newNotes = this.state.notes.slice();
+        newNotes.unshift(newNote);
+        this.setState({ notes: newNotes });
+    }
+    
+    render() {
+        return (
+            <div>
+               <NoteEdinor
+                   onNoteAdd={this.handleNoteAdd.bind(this)} 
+               />
+               <NotesGrid notes={this.state.notes}/>
+            </div>
+        );
+    }
+}
+
+export default SmartNotes;
---
+++ src/component/smart-notes/smartNotesRout.js
@@ -0,0 +1,3 @@
+/**
+ * Created by bad4iz on 02.06.2017.
+ */
