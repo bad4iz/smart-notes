f3d6517c203557003e4895a623167ab00567b4b5
Евгений Плотников <bad4iz@gmail.com>
2017-06-05 15:34:46 +0400

    yyy

--- src/pages/smart-notes/Index.jsx
+++ src/pages/smart-notes/Index.jsx
@@ -1,3 +1,31 @@
-/**
- * Created by bad4iz on 05.06.2017.
- */
+import React, { Component } from 'react';
+
+
+import NoteEdinor from './noteEditor/NoteEditor';
+import NotesGrid from './notesGrid/NotesGrid';
+
+class Index extends Component {
+    static path = '/smart-notes';
+    state = {
+        notes: []
+    };
+
+    handleNoteAdd(newNote) {
+        const newNotes = this.state.notes.slice();
+        newNotes.unshift(newNote);
+        this.setState({ notes: newNotes });
+    }
+
+    render() {
+        return (
+            <div>
+                <NoteEdinor
+                    onNoteAdd={this.handleNoteAdd.bind(this)}
+                />
+                <NotesGrid notes={this.state.notes}/>
+            </div>
+        );
+    }
+}
+
+export default Index;
