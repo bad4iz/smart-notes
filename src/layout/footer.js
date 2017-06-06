72fa596f41e054039f2342eb95de652929324813
bad4iz <bad4iz@gmail.com>
2017-06-06 22:04:21 +0400

    logo and const

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Footer as MDLFooter, FooterSection, FooterLinkList} from 'react-mdl';
 
 
-class SmartFooter extends Component {
-    state = {}
-    render() {
-        return (
-            <MDLFooter size="mini">
-                <FooterSection type="left" logo="Title">
-                    <FooterLinkList>
-                        <a href="#Help">Help</a>
-                        <a href="#Privacy">Privacy & Terms</a>
-                    </FooterLinkList>
-                </FooterSection>
-            </MDLFooter>
-        );
-    }
-}
+const Footer = () => (
+        <MDLFooter size="mini">
+            <FooterSection type="left" logo="Bad4iz">
+                <FooterLinkList>
+                    <a href="#Help">Help</a>
+                    <a href="#Privacy">Privacy & Terms</a>
+                </FooterLinkList>
+            </FooterSection>
+        </MDLFooter>
+    );
 
-export default SmartFooter;
+export default Footer;
 
