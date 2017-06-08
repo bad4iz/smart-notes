ab33b3ff0cb91091cb463d85e7d3446a994c1044
bad4iz <bad4iz@gmail.com>
2017-06-08 18:50:14 +0400

    jhkj (reverted from commit 38bc98184a7e4bf3673aa4064039a222665c01eb)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
+//
+// import { Layout } from 'react-mdl';
+//
+// import Header from './layout/header';
+// import Content from './layout/content';
+// import Drawer from './layout/drawer';
+// import Footer from './layout/footer';
 
-import { Layout } from 'react-mdl';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 
-import Header from './layout/header';
-import Content from './layout/content';
-import Drawer from './layout/drawer';
-import Footer from './layout/footer';
+import {cyan500} from 'material-ui/styles/colors';
+const muiTheme = getMuiTheme({
+    palette: {
+        textColor: cyan500,
+    },
+    appBar: {
+        height: 50,
+    },
+});
 
 class App extends Component {
     state = {
@@ -17,15 +32,19 @@ class App extends Component {
     render() {
         return (
           <div className="App">
-           
-           <Layout style={{ background: 'url(https://www.firestock.ru/wp-content/uploads/2014/09/photodune-1464507-700x466.jpg) center / cover' }}>
-                <Header title={this.state.title}/>
-                <Drawer title={this.state.title} />
-                <Content >
-                  { this.props.children }
-                </Content>
-              <Footer title={this.state.title}/>
-            </Layout>
+              <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                  <AppBar title="My AppBar" />
+              </MuiThemeProvider>
+             
+            {/**/}
+           {/*<Layout style={{ background: 'url(https://www.firestock.ru/wp-content/uploads/2014/09/photodune-1464507-700x466.jpg) center / cover' }}>*/}
+                {/*<Header title={this.state.title}/>*/}
+                {/*<Drawer title={this.state.title} />*/}
+                {/*<Content >*/}
+                  {/*{ this.props.children }*/}
+                {/*</Content>*/}
+              {/*<Footer title={this.state.title}/>*/}
+            {/*</Layout>*/}
           </div >
         );
     }
